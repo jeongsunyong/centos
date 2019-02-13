15d49eb4d6b60bb86b93a7cc83eb64d93f64b746
jeongsunyong <sunyong0321@naver.com>
2019-02-13 11:16:19 +0900

    blender tar옵션 xjf

--- scripts/blender.sh
+++ scripts/blender.sh
@@ -4,5 +4,5 @@ if [ ! -d "/home/$USER/app" ]; then
 fi
 cd /home/$USER/app
 wget https://www.blender.org/download/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2/
-tar blender-2.79b-linux-glibc219-x86_64.tar.bz2/
+tar -xjf blender-2.79b-linux-glibc219-x86_64.tar.bz2/
  
