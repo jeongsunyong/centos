03726f4aba0ace6dbe3fafabf821e69838694bc4
jeongsunyong <sunyong0321@naver.com>
2019-02-13 14:09:06 +0900

    alembic경로수정

--- scripts/alembic.sh
+++ scripts/alembic.sh
@@ -11,6 +11,6 @@ cd ..
 mkdir alembic_build
 mkdir alembic
 cd alembic_build
-~/app/cmake-3.13.3/bin/cmake ../alembic_src -DILMBASE_ROOT=$HOME/app/IlmBase -DALEMBIC_SHARED_LIBS=OFF -DUSE_HDF5=OFF -DALEMBIC_LIB_USES_TR1=ON -DCMAKE_INSTALL_PREFIX=$HOME/app/alembic
+/home/$USER/app/cmake-3.13.3/bin/cmake ../alembic_src -DILMBASE_ROOT=/home/$USER/app/IlmBase -DALEMBIC_SHARED_LIBS=OFF -DUSE_HDF5=OFF -DALEMBIC_LIB_USES_TR1=ON -DCMAKE_INSTALL_PREFIX=/home/$USER/app/alembic
 make -j2
 make install
