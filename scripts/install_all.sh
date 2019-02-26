ab037ba4d6f37d0d24c97758f774f73136a6e76c
jeongsunyong <sunyong0321@naver.com>
2019-02-26 16:43:39 +0900

    install_all 수정

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,18 +1,41 @@
 #! /bin/install_all.sh
+
+# OS : centos 7.6
+# after running as root, please run again as user.
+
+#execute as root
+if [[ $EUID -e 0]]; then
+
+cd /home/$USER/centos/scripts/root/
 nvidia.sh
 utility.sh
+pip_install.sh
+install.sh
+devtools.sh
 code.sh
 djv.sh
 mpv.sh
-rmbeep.sh
-trans.sh
-pip.sh
-devtools.sh
-FFmpeg.sh
 handbrake.sh
+openimageio.sh
+nohotcorner.sh
+rmbeep.sh
+redis.sh
+mongoDB.sh
+postgreSQL.sh
 
-#
-scl enable devtoolset-6 ./cmake.sh
-scl enable devtoolset-6 ./openexr.sh
-scl enable devtoolset-6 ./opencolorio.sh
-scl enable devtoolset-6 ./alembic.sh
+else
+
+#excute as user
+cd /home/$USER/centos/scripts/user/
+pip.sh
+ffmpeg.sh
+blender.sh
+gaffer.sh
+ocioconfig.sh
+trans.sh
+scl enable devtoolset-6 /home/$USER/centos/scripts/user/cmake.sh
+scl enable devtoolset-6 /home/$USER/centos/scripts/user/openexr.sh
+scl enable devtoolset-6 /home/$USER/centos/scripts/user/opencolorio.sh
+scl enable devtoolset-6 /home/$USER/centos/scripts/user/alembic.sh
+
+fi
